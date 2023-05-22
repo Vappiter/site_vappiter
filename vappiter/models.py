8dcadfb0b84e757412d02da3c538fdf10193a1a9
Vappiter <vappiter@yandex.ru>
2023-05-22 18:22:35 +0300

    0.0.0.6

--- vappiter/models.py
+++ vappiter/models.py
@@ -5,9 +5,13 @@ class Country(models.Model):
     country = models.CharField(max_length=100, verbose_name='Страна')
 
     def __str__(self) -> str:
-        return self.name_country
+        return self.country
     
 class Company(models.Model):
     """Производители оборудования"""
     company = models.CharField(max_length=100, verbose_name='Компания')
     country = models.ForeignKey(Country, on_delete=models.CASCADE, verbose_name='Страна')
+        
+
+    def __str__(self) -> str:
+        return self.company
