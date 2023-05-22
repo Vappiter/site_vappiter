e6bdc53bd4a81bb53b74bbc02b63b171cc495333
Vappiter <vappiter@yandex.ru>
2023-05-22 16:48:51 +0300

    111

--- vappiter/models.py
+++ vappiter/models.py
@@ -1,8 +1,13 @@
-from django.db import models
+from django.db import models;
 
 class Country(models.Model):
     """Страны производители оборудования"""
-    name_country = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
 
     def __str__(self) -> str:
         return self.name_country
+    
+class Company(models.Model):
+    """Производители оборудования"""
+    company = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
