3e19467451c9a1660a3f83295de536bb72605d28
Andrey Vladimirov <vappiter@yandex.ru>
2023-05-29 23:04:17 +0300

    006-2023-05-29

--- vappiter/admin.py
+++ vappiter/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from vappiter.models import Country, Company, Product   
+from vappiter.models import Country, Company, Product, Nameproduct   
 
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
@@ -12,8 +12,14 @@ class CompanyAdmin(admin.ModelAdmin):
     list_display = ('company','country')
     pass
 
+@admin.register(Nameproduct)
+class NameproductAdmin(admin.ModelAdmin):
+    list_display = ('nameproduct')
+    pass
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('company','title_product','type_product')
+    list_display = ('company','nameproduct','type_product')
     pass
 # admin.site.register()
